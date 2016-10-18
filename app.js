503159469ff414279c8e4f925b0d7bbb1f65927b
Kelvin Tan <kelvin.tyb@gmail.com>
2016-10-18 17:28:47 +0800

    deployment config

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ var bodyParser = require('body-parser')
 
 var mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/artery')
+  //NOTE: process.env.NODE_ENV is your node js env
+if (process.env.NODE_ENV === 'production') {
+  mongoose.connect('mongodb://closecl:3360507@ds061076.mlab.com:61076/artery')
+} else {
+  mongoose.connect('mongodb://localhost/artery')
+}
 
 app.set('view engine', 'ejs')
 app.use(layout)
@@ -21,5 +26,7 @@ var userRoutes = require('./routes/users')
 
 app.use('/users', userRoutes)
 
-app.listen(3000)
+
+//listen to port from Heroku or 3000 for local testing
+app.listen(process.env.PORT || 3000)
 console.log('Server started')
