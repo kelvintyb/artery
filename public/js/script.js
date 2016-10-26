5301a60429d0dfe93cf00ad871a377990860b635
Kelvin Tan <kelvin.tyb@gmail.com>
2016-10-26 21:19:55 +0800

    finished search with user details

--- public/js/script.js
+++ public/js/script.js
@@ -1,63 +1,79 @@
 $(document).ready(function($) {
 
-      //ajax logic for curator.ejs - http://stackoverflow.com/questions/20321291/ajax-call-to-from-mongodb-example-for-node-express
-      //when ready to refactor to ajax, go to curator ejs and take out form element
-      // var $thumbsUp = $('.thumbs-up'); <a href="#" class='thumbs-up'> to be placed back into curator.ejs
-      // var $thumbsDown = $('.thumbs-down');
-      // var randomiser = function (arr) {
-      //   var rand = Math.floor(Math.random() * arr.length);
-      //   return arr[rand];
-      // }
-      // $thumbsUp.on('click',function(e){
-      //   e.preventDefault();
-      // })
-      // $thumbsDown.on('click',function(e){
-      //   e.preventDefault();
-      //   $('.paintingContainer').remove();
-      //   $('.artshow').append()
-      // })
+  //ajax logic for curator.ejs - http://stackoverflow.com/questions/20321291/ajax-call-to-from-mongodb-example-for-node-express
+  //when ready to refactor to ajax, go to curator ejs and take out form element
+  // var $thumbsUp = $('.thumbs-up'); <a href="#" class='thumbs-up'> to be placed back into curator.ejs
+  // var $thumbsDown = $('.thumbs-down');
+  // var randomiser = function (arr) {
+  //   var rand = Math.floor(Math.random() * arr.length);
+  //   return arr[rand];
+  // }
+  // $thumbsUp.on('click',function(e){
+  //   e.preventDefault();
+  // })
+  // $thumbsDown.on('click',function(e){
+  //   e.preventDefault();
+  //   $('.paintingContainer').remove();
+  //   $('.artshow').append()
+  // })
 
-      //ajax calls for search.ejs
-      $('#searchName').on('submit', function(e) {
-        e.preventDefault()
-        var query = $(this).serializeArray()[0].value;
-        $.get({
-          url: '/api/paintings/name/' + query,
-          error: function() {
-            $("#search-painting-list").html('<br><p style="color: red">No painting found. Please try again.</p>')
-          }
-        }).done(appendPaintings)
-      })
+  //ajax calls for navbar
+  $('#curate-btn').on('click',function(e){
+    e.preventDefault();
+    window.alert('yo')
+  })
 
-      $('#searchCategory').on('submit', function(e) {
-        e.preventDefault()
-        var query = $(this).serializeArray()[0].value;
-        $.get({
-          url: '/api/paintings/category/' + query,
-          error: function() {
-            $("#search-painting-list").html('<br><p style="color: red">No painting found. Please try again.</p>')
-          }
-        }).done(appendPaintings)
-      })
+  //ajax calls for search.ejs
+  $('#searchName').on('submit', function(e) {
+    e.preventDefault()
+    var query = $(this).serializeArray()[0].value;
+    $.get({
+      url: '/api/paintings/name/' + query,
+      error: function() {
+        $("#search-painting-list").html('<br><p style="color: red">No painting found. Please try again.</p>')
+      },
+      success: appendPaintings
+    })
+  })
 
-      $('#searchArtist').on('submit', function(e) {
-        e.preventDefault()
-        var query = $(this).serializeArray()[0].value;
-        $.get({
-          url: '/api/paintings/artist/' + query,
-          error: function() {
-            $("#search-painting-list").html('<br><p style="color: red">No painting found. Please try again.</p>')
-          },
-          success:appendPaintings
-        })
+  $('#searchCategory').on('submit', function(e) {
+    e.preventDefault()
+    var query = $(this).serializeArray()[0].value;
+    $.get({
+      url: '/api/paintings/category/' + query,
+      error: function() {
+        $("#search-painting-list").html('<br><p style="color: red">No painting found. Please try again.</p>')
+      },
+      success: appendPaintings
+    })
+  })
+
+  $('#searchArtist').on('submit', function(e) {
+    e.preventDefault()
+    var query = $(this).serializeArray()[0].value;
+    $.get({
+      url: '/api/paintings/artist/' + query,
+      error: function() {
+        $("#search-painting-list").html('<br><p style="color: red">Please try again.</p>')
+      },
+      success: appendPaintings
+    })
+  })
+
+  function appendPaintings(paintingsArr) {
+    if (paintingsArr.length == 0) {
+      $("#search-painting-list").html('<br><p style="color: red">No painting found. Please try again.</p>')
+    } else {
+      $("#search-painting-list").empty();
+      //BUG:append is overwriting for some reason, solved by using success func in ajax call instead of .done promise
+      paintingsArr.forEach(function(painting) {
+        console.log(painting.ownedBy.local.name)
+        var insertStr = "<div class='col-md-4 painting'><a href=" + painting.permalink + " target='_blank'>Name: " + painting.name + "<br>Category: " + painting.category + "<br>Artist: " + painting.artist + "<br><img class='img-thumbnail' src=" + painting.imageUrl + " style='max-width:18.75em;max-height:25em'>" + "</a><br>Owned by: " + painting.ownedBy.local.name + "</div>";
+        $("#search-painting-list").append(insertStr)
       })
-      function appendPaintings(paintingsArr) {
-        // console.log(paintingsArr)
-        $("#search-painting-list").empty();
-        //append is overwriting for some reason
-        paintingsArr.forEach(function(painting){
-          // console.log(painting)
-          $("#search-painting-list").append("<div class='col-md-4 painting'><a href=" + painting.permalink + ">Name: " + painting.name + "<br>Category: " + painting.category + "<br>Artist: " + painting.artist + "<br><img class='img-thumbnail' src=" + painting.imageUrl + " alt=" + painting.name + " style='max-width:18.75em;max-height:25em'><br>Price: " + painting.price + "<br></a></div>")
-        })
-      }
+    }
+  }
+
+
+
 })
--- routes/paintings_api.js
+++ routes/paintings_api.js
@@ -13,26 +13,28 @@ function authPass(req, res, next) {
 }
 //NOTE: below is a temp solution to handling multi-category search, ideally shld handle /:id and check :id against a table to determine if it is a painting name, category or artist
 router.get('/name/:name', function(req, res) {
-  Painting.find({
-    'name': req.params.name
-  }, function(err, paintings) {
-    res.json(paintings)
-  })
+  var query = {'name': req.params.name};
+  Painting.find(query)
+          .populate("ownedBy")
+          .exec(function(err, paintings) {
+          res.json(paintings)
+        })
 })
 router.get('/category/:category', function(req, res) {
-  Painting.find({
-    'category': req.params.category
-  }, function(err, paintings) {
-    res.json(paintings)
-  })
+  var query = {'category': req.params.category};
+  Painting.find(query)
+          .populate("ownedBy")
+          .exec(function(err, paintings) {
+          res.json(paintings)
+        })
 })
 router.get('/artist/:artist', function(req, res) {
-  Painting.find({
-    'artist': req.params.artist
-  }, function(err, paintings) {
-    console.log(paintings)
-    res.json(paintings)
-  })
+  var query = {'artist': req.params.artist};
+  Painting.find(query)
+          .populate("ownedBy")
+          .exec(function(err, paintings) {
+          res.json(paintings)
+        })
 })
 
 router.post('/updatelikes', authPass, function(req, res) {
