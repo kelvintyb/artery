42cfd088abcb158014efd714d24bfe57c27d6553
Kelvin Tan <kelvin.tyb@gmail.com>
2016-10-26 18:47:09 +0800

    finished searching function

--- public/js/script.js
+++ public/js/script.js
@@ -1,76 +1,63 @@
 $(document).ready(function($) {
 
-  //this logic is for ajax calls only
-  var $userForm = $('.new-user')
+      //ajax logic for curator.ejs - http://stackoverflow.com/questions/20321291/ajax-call-to-from-mongodb-example-for-node-express
+      //when ready to refactor to ajax, go to curator ejs and take out form element
+      // var $thumbsUp = $('.thumbs-up'); <a href="#" class='thumbs-up'> to be placed back into curator.ejs
+      // var $thumbsDown = $('.thumbs-down');
+      // var randomiser = function (arr) {
+      //   var rand = Math.floor(Math.random() * arr.length);
+      //   return arr[rand];
+      // }
+      // $thumbsUp.on('click',function(e){
+      //   e.preventDefault();
+      // })
+      // $thumbsDown.on('click',function(e){
+      //   e.preventDefault();
+      //   $('.paintingContainer').remove();
+      //   $('.artshow').append()
+      // })
 
-  $userForm.on('submit', function(e) {
-    e.preventDefault()
-    var formdata = $(this).serializeArray()
-    window.alert('ajax call now')
-    $.post({
-      url: '/api/users',
-      data: formdata
-    }).done(doSomething)
-  })
+      //ajax calls for search.ejs
+      $('#searchName').on('submit', function(e) {
+        e.preventDefault()
+        var query = $(this).serializeArray()[0].value;
+        $.get({
+          url: '/api/paintings/name/' + query,
+          error: function() {
+            $("#search-painting-list").html('<br><p style="color: red">No painting found. Please try again.</p>')
+          }
+        }).done(appendPaintings)
+      })
 
-  function doSomething(data) {
-    console.log(data);
-    window.alert('form submitted, new users created')
-    $('#all-user-list').append('<li>' + data.local.name + '<br>' + data.local.email + '<br>' + data.local.password + '</li>')
-  }
+      $('#searchCategory').on('submit', function(e) {
+        e.preventDefault()
+        var query = $(this).serializeArray()[0].value;
+        $.get({
+          url: '/api/paintings/category/' + query,
+          error: function() {
+            $("#search-painting-list").html('<br><p style="color: red">No painting found. Please try again.</p>')
+          }
+        }).done(appendPaintings)
+      })
 
-  //ajax logic for curator.ejs - http://stackoverflow.com/questions/20321291/ajax-call-to-from-mongodb-example-for-node-express
-  //when ready to refactor to ajax, go to curator ejs and take out form element
-  // var $thumbsUp = $('.thumbs-up'); <a href="#" class='thumbs-up'> to be placed back into curator.ejs
-  // var $thumbsDown = $('.thumbs-down');
-  // var randomiser = function (arr) {
-  //   var rand = Math.floor(Math.random() * arr.length);
-  //   return arr[rand];
-  // }
-  // $thumbsUp.on('click',function(e){
-  //   e.preventDefault();
-  // })
-  // $thumbsDown.on('click',function(e){
-  //   e.preventDefault();
-  //   $('.paintingContainer').remove();
-  //   $('.artshow').append()
-  // })
-
-  //ajax call for search.ejs
-  $('#searchName').on('submit', function(e) {
-    e.preventDefault()
-    var query = $(this).seralize();
-    console.log(query);
-    //
-    // $.post({
-    //   url: '/api/users',
-    //   data: formdata
-    // }).done(appendPainting)
-  })
-
-  function appendPainting(data) {
-    // $('#search-painting-list').append('<li>' + data.local.name + '<br>' + data.local.email + '<br>' + data.local.password + '</li>')
-    console.log(data)
-  }
-
-  // $.ajax({
-  //      url: '/api/paintings',
-  //      data: {
-  //         format: 'json'
-  //      },
-  //      error: function() {
-  //         $('#info').html('<p>An error has occurred</p>');
-  //      },
-  //      dataType: 'jsonp',
-  //      success: function(data) {
-  //         var $title = $('<h1>').text(data.talks[0].talk_title);
-  //         var $description = $('<p>').text(data.talks[0].talk_description);
-  //         $('#info')
-  //            .append($title)
-  //            .append($description);
-  //      },
-  //      type: 'GET'
-  //   });
-
-
+      $('#searchArtist').on('submit', function(e) {
+        e.preventDefault()
+        var query = $(this).serializeArray()[0].value;
+        $.get({
+          url: '/api/paintings/artist/' + query,
+          error: function() {
+            $("#search-painting-list").html('<br><p style="color: red">No painting found. Please try again.</p>')
+          },
+          success:appendPaintings
+        })
+      })
+      function appendPaintings(paintingsArr) {
+        // console.log(paintingsArr)
+        $("#search-painting-list").empty();
+        //append is overwriting for some reason
+        paintingsArr.forEach(function(painting){
+          // console.log(painting)
+          $("#search-painting-list").append("<div class='col-md-4 painting'><a href=" + painting.permalink + ">Name: " + painting.name + "<br>Category: " + painting.category + "<br>Artist: " + painting.artist + "<br><img class='img-thumbnail' src=" + painting.imageUrl + " alt=" + painting.name + " style='max-width:18.75em;max-height:25em'><br>Price: " + painting.price + "<br></a></div>")
+        })
+      }
 })
--- routes/paintings_api.js
+++ routes/paintings_api.js
@@ -4,41 +4,66 @@ var passport = require('passport')
 var User = require('../models/user')
 var Painting = require('../models/painting')
 
-function authPass(req,res,next){
-  if(req.isAuthenticated()){
+function authPass(req, res, next) {
+  if (req.isAuthenticated()) {
     return next();
   } else {
     return res.redirect('/login');
   }
 }
+//NOTE: below is a temp solution to handling multi-category search, ideally shld handle /:id and check :id against a table to determine if it is a painting name, category or artist
+router.get('/name/:name', function(req, res) {
+  Painting.find({
+    'name': req.params.name
+  }, function(err, paintings) {
+    res.json(paintings)
+  })
+})
+router.get('/category/:category', function(req, res) {
+  Painting.find({
+    'category': req.params.category
+  }, function(err, paintings) {
+    res.json(paintings)
+  })
+})
+router.get('/artist/:artist', function(req, res) {
+  Painting.find({
+    'artist': req.params.artist
+  }, function(err, paintings) {
+    console.log(paintings)
+    res.json(paintings)
+  })
+})
 
-router.route('/updatelikes')
-      .post(authPass, function(req, res){
-        var currPaintingScore = 0,
-            categoryHelper = {
-              "Renaissance": 1,
-              "Post-Impressionism": 2,
-              "Cubism": 3
-            };
-        Painting.findById(req.body.paintingId, function(err, model){
-          console.log(model);
-          if (err) throw new Error(err);
-          model.likes += 1;
-          currPaintingScore = categoryHelper[model.category]
-          model.save();
-        })
-        User.findById(req.user.id,function(err,model){
-          if (err) throw new Error(err);
-          //calculate avg cat score
-          model.categoryScore = Math.round((model.categoryScore + currPaintingScore) / (model.likeList.length + 1))
-          model.likes += 1;
-          model.save();
-        })
-        User.findByIdAndUpdate(req.user.id,{$addToSet:{"likeList":req.body.paintingId}}, function(err,model){
-          if (err) throw new Error(err);
-          res.redirect('/curator');
-        })
-      })
-
+router.post('/updatelikes', authPass, function(req, res) {
+  var currPaintingScore = 0,
+    categoryHelper = {
+      "Renaissance": 1,
+      "Post-Impressionism": 2,
+      "Cubism": 3
+    };
+  Painting.findById(req.body.paintingId, function(err, model) {
+    console.log(model);
+    if (err) throw new Error(err);
+    model.likes += 1;
+    currPaintingScore = categoryHelper[model.category]
+    model.save();
+  })
+  User.findById(req.user.id, function(err, model) {
+    if (err) throw new Error(err);
+    //calculate avg cat score
+    model.categoryScore = Math.round((model.categoryScore + currPaintingScore) / (model.likeList.length + 1))
+    model.likes += 1;
+    model.save();
+  })
+  User.findByIdAndUpdate(req.user.id, {
+    $addToSet: {
+      "likeList": req.body.paintingId
+    }
+  }, function(err, model) {
+    if (err) throw new Error(err);
+    res.redirect('/curator');
+  })
+})
 
 module.exports = router;
